792a90bae0b98b9dd0aa6eaa0b1f5ee852ee3bb5
Kapil Baidya <kapilbaidya02>
2024-05-13 03:33:33 +0600

    add new line

--- Escape Sequence.java
+++ Escape Sequence.java
@@ -1,1 +1,9 @@
-
+class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.print("Kapil \nBSc in CSE\n");
+        System.out.print("Southeast University");
+        System.out.print("\nDay \t 1 \n");
+        System.out.print("Day \t 2");
+        System.out.println("\nDay \t 3");
+    }
+}
