5a7fb34cbc697aee265722f74350e2f54dfbccfb
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-14 03:53:13 +0600

    Update Comments.java

--- Comments.java
+++ Comments.java
@@ -1,1 +1,23 @@
+public class Comments {
 
+    public static void main(String[] args) {
+
+        System.out.println("Comments in Java");
+        System.out.println("Single line comments");
+         //displaying my learning period
+        System.out.println("Multi line comments");
+         /*
+          program name:
+          programmer name:
+          date:
+          */
+        System.out.println("Documentational comments");
+        /**
+         * 
+         * 
+         * 
+         */
+
+
+    }
+}
