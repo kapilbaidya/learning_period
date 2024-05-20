b853d00bb4386aff8b2752440a6b8410122b2083
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-20 21:35:51 +0600

    Update and rename Phases.java to Variables.java
    add new line

---
+++ Variables.java
@@ -0,0 +1,23 @@
+class Variables{
+    public static void main (String [] args){
+      //String variables
+      String name = "Kapil";
+      System.out.println(name);
+      
+      //int variables
+      int age = 22;
+      System.out.println(age);
+      
+      //float variables
+      float money = 100.02f;
+      System.out.println(money);
+
+      //char variables
+      char section = 'A';
+      System.out.println(section);
+
+      //boolean variables
+      boolean student = true;
+      System.out.println(student);
+        }
+  }
