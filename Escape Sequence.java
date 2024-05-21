f0bf15fa09b72bbe6aed4321a2e57486c0e331e4
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-22 03:23:33 +0600

    Update and rename Escape Sequence.java to Variables  .java

--- Escape Sequence.java
+++
@@ -1,9 +0,0 @@
-class EscapeSequence {
-    public static void main(String[] args) {
-        System.out.print("Kapil \nBSc in CSE\n");
-        System.out.print("Southeast University");
-        System.out.print("\nDay \t 1 \n");
-        System.out.print("Day \t 2");
-        System.out.println("\nDay \t 3");
-    }
-}
