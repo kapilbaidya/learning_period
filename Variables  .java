576e0f149a332d0eca252af9c89d8702cd198a21
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-22 03:33:48 +0600

    Update Variables  .java

--- Variables  .java
+++ Variables  .java
@@ -1,1 +1,13 @@
+class Variables{
+  public static void main(String[]args){
+    String firstName = "kapi";
+    String lastName = "Baidya";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);
 
+    int x = 9;
+    int y = 11;
+    int a = x + y;
+    System.out.println(a);
+  }
+}
