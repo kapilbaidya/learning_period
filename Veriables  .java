a65c0c6f18e2b6e49d0f67c9bd47594aaa960364
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-22 02:24:30 +0600

    Update and rename Veriable and Data Type to Veriables  .java

---
+++ Veriables  .java
@@ -0,0 +1,25 @@
+class Veriables {
+  public static void main(String[]args){
+    String name;
+    name = "kapil";
+    System.out.println(name);
+
+    int age;
+    age = 22;
+    System.out.println(age);
+
+    float height;
+    height = 5.5f;
+    System.out.println(height);
+
+    char blood;
+    blood = 'B';
+    System.out.println(blood);
+
+    boolean boy;
+    boy = true;
+    girl = false;
+    system.out.println(boy);
+  }
+}
+
