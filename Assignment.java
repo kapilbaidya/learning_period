c45d7127b1964b60bf438b0959e647445245787d
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-23 02:45:54 +0600

    Update and rename Assignment.java to Variables Assignment.java

--- Assignment.java
+++
@@ -1,8 +0,0 @@
-public class Assignment {
-    public static void main(String[] args) {
-        System.out.println("Id: 101");
-        System.out.println("Title: Iphone15");
-        System.out.println("Price: 1895 euros");
-        System.out.print("Category: Phone");
-    }
-}
---
+++ Variables Assignment.java
@@ -0,0 +1,17 @@
+class Variables {
+    public static void main(String[]args) {
+        //Student Data
+        String name = "Kapil Baidya";
+        int id = 15;
+        int age = 22;
+        float point = 3.57f;
+        char grade = 'A';
+
+        //Print Variables
+        System.out.println("Student Name:" + name);
+        System.out.println("Student Id: " + id);
+        System.out.println("Student Age: " + age);
+        System.out.println("Student Point: " + point);
+        System.out.println("Student Grade: " + grade);
+    }
+}
