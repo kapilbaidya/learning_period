756a3436575c7c952fd90d11f0bb677038c9e3f2
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-23 02:58:04 +0600

    Create Variables Assignment .java

---
+++ Variables Assignment .java
@@ -0,0 +1,16 @@
+class Variables{
+  public static void main(String[]args){
+  //Create integeer variables
+  int length = 5;
+  int width = 10;
+  int area;
+
+  //Calculate the area of a rectangle
+area = length * width;
+
+//print variables
+  System.out.println("Length is : " + length);
+  System.out.println("Width is : " + width);
+  System.out.println("Area of the rectangle is : " + area);
+  }
+}
