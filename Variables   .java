56006eae4fc360881caf8fbefc38b50e6ed96697
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-23 01:39:24 +0600

    Update Variables   .java

--- Variables   .java
+++ Variables   .java
@@ -1,11 +1,5 @@
 class Variables {
   public static void main (String[]args) {
-    int x = 5;
-    int y = 10;
-    int z = 20;
-    int a = x + y + z;
-    System.out.println(a);
-
     int a, b, c;
     a = b = c = 50;
     System.out.println(a + b + c);
