4713e19ff9e3d605f7039698e2cef71a34c9fec8
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-24 01:58:23 +0600

    Create Data Type .java

---
+++ Data Type .java
@@ -0,0 +1,22 @@
+class DataType{
+  public static void main (String[]args) {
+  
+    byte a = 100;
+    System.out.println(a);
+
+    short b = 5000;
+    System.out.println(b);
+
+    int c = 100000;
+    System.out.println(c);
+
+    long d = 100000l;
+    System.out.println(d);
+
+    float x = 9999.99f;
+    System.out.println(x);
+
+    double y = 100000000000.01d;
+    System.out.println(y);
+  }
+}
