395f72c3a139667f9e8222fb36084da486e013c9
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-24 01:28:32 +0600

    Create DataType.java

---
+++ DataType.java
@@ -0,0 +1,15 @@
+class DataType {
+  public static void main(String[]args) {
+    String myName = "Kapil";
+    int myAge = 22;
+    float myHeight = 5.5f;
+    char mySection = 'A';
+    boolean myAns = true;
+
+    System.out.println(myName);
+    System.out.println(myAge);
+    System.out.println(myHeight);
+    System.out.println(mySection);
+    System.out.println(myAns);
+  }
+}
