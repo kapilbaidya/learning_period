66b098ed076c196b0ba19edeb47d1491ba61b12e
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-26 02:26:19 +0600

    Create Data Type.java

---
+++ Data Type.java
@@ -0,0 +1,16 @@
+class DataType {
+  public static void main(String[]args) {
+
+    char asCii1 = 75;
+    char asCii2 = 65;
+    char asCii3 = 80;
+    char asCii4 = 73;
+    char asCii5 = 76;
+
+    System.out.print(asCii1);
+    System.out.print(asCii2);
+    System.out.print(asCii3);
+    System.out.print(asCii4);
+    System.out.print(asCii5);
+  }
+}
