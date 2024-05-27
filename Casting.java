dd8e028ef8828139cb6de877718e4ae8be562e4d
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-27 22:54:02 +0600

    Create Casting.java

---
+++ Casting.java
@@ -0,0 +1,10 @@
+class Casting {
+  public static void main(String[]args){
+    //Widening Casting
+    int myInt = 59;
+    double myDouble = myInt;
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }
+}
