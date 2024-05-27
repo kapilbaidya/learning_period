409613eeab328af4a4fd7e798d3d3e3b568603de
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-28 00:47:06 +0600

    Create Casting Example.java

---
+++ Casting Example.java
@@ -0,0 +1,10 @@
+class CastingExample{
+  public static void main (String[]args){
+    int totalFee = 27000;
+    int paidFee = 13035;
+    float percentage = (float) paidFee / totalFee * 100.0f;
+
+
+  System.out.println("Paid Fee: " + percentage );
+  }
+}
