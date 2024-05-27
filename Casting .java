5bc6c005aae411b0552c3e96db449444936df99f
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-05-27 23:16:53 +0600

    Create Casting .java

---
+++ Casting .java
@@ -0,0 +1,10 @@
+class Casting {
+  public static void main(String[]args){
+    //Narrowing Casting
+    double myDouble = 99.99d;
+    int myInt = (int) myDouble;
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
