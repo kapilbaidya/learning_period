836d1c839a9b799b66de58deb7b26ba2d3fc1951
Kapil Baidya <143291369+kapilbaidya@users.noreply.github.com>
2024-06-01 03:19:15 +0600

    Update and rename nextclass .java to Operators .java
    add new line

---
+++ Operators .java
@@ -0,0 +1,21 @@
+class Operatos{
+  public static void main(String[]args){
+    //arithmetic operators
+    int x = 10;
+    int y = 20;
+    int addition = x + y;
+    System.out.println(addition);
+    int a = 29;
+    int b = 31;
+    int sub = a - b;
+    System.out.println(sub);
+    int xx = 5;
+    int yy = 3;
+    System.out.println(x * y);
+    int e = 12;
+    int f = 3;
+    System.out.println(x / y);
+    
+  }
+}
+    
